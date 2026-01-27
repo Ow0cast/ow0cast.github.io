db2cda3cb49ff375af0e81b566eef23a35ae5237
owuh <owuh@wayland.best>
2026-01-27 00:17:52 -0500

    i guess i need this

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        server: {
-            allowedHosts: ['owuh.dev']
-        }
-    }
+  vite: {
+      server: {
+          allowedHosts: ['owuh.dev']
+      }
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
